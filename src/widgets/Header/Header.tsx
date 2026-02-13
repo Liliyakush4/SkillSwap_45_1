ec1608e8767e6a2a324c120cab5a0951e304f339
Liliya Kushnikova <iamlilkush4@gmail.com>
2026-02-13 03:31:38 +0300

    chore(styles): cleanup global styles and remove unused global classes

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const Header: React.FC = () => {
-  return (
-    <header>
-        { /* содержимое */}
-    </header>
-  )
-}
+  return <header>{/* содержимое */}</header>;
+};
 
-export default Header
+export default Header;
