965064332551065d5e1f20f6847bfb3acfc10d6c
Egor-developer <puzyrevegor452@gmail.com>
2026-02-13 15:17:15 +0300

    fix: color hover

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,10 +1,7 @@
-import { FileDropzone } from '../../shared/ui/file-dropzone';
-
 export default function MainPage() {
   return (
     <div>
       <h1>Главная страница</h1>
-      <FileDropzone />
     </div>
   );
 }
