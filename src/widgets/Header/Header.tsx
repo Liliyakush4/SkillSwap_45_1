533458db9df5c2cf80b85c585820157fd5a0857b
Ekaterina Kharina <Ekaterina.Kharina@waveaccess.ru>
2026-02-15 15:01:39 +0300

    разрешение конфликта

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 const Header: React.FC = () => {
-  return <header>{/* содержимое */}
-  </header>;
+  return <header>{/* содержимое */}</header>;
 };
 
 export default Header;
