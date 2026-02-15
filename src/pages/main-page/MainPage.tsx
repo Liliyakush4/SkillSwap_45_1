c761b499d17c4497cd22d5d9217bc259f828a724
Liliya Kushnikova <iamlilkush4@gmail.com>
2026-02-15 03:42:04 +0300

    доработано модальное окно offer и компонент Modal

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+import { OfferCreatedModal } from "../../widgets/modals/OfferCreatedModal";
+
 export default function MainPage() {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
     <div>
       <h1>Главная страница</h1>
+      <button type="button" onClick={() => setIsOpen(true)}>
+        Открыть модалку
+      </button>
+
+      <OfferCreatedModal
+      isOpen={isOpen}
+      onClose={() => setIsOpen(false)}
+       />
     </div>
   );
 }
