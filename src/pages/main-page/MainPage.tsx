970e54c3bbb161a96d7da9cc3d4ca6602cc08406
Ekaterina Kharina <Ekaterina.Kharina@waveaccess.ru>
2026-02-16 18:29:11 +0300

    убрала лишнее

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,11 +1,8 @@
-import { AvatarUploader } from "../../shared/ui/avatar-uploader/AvatarUploader";
-
 export default function MainPage() {
 
   return (
     <div>
       <h1>Главная страница</h1>
-      <AvatarUploader/>
     </div>
   );
 }
