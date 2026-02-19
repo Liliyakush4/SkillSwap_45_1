6eccfa8f27fb06cfb334877baee2da987fc939fd
veraulitinaYa <veraulitina@yandex.ru>
2026-02-19 22:59:03 +0300

    fix: header buttons

--- src/widgets/Header/ui/header-base/HeaderBase.tsx
+++ src/widgets/Header/ui/header-base/HeaderBase.tsx
@@ -7,10 +7,16 @@ export const HeaderBase = () => {
       <Logo />
 
       <nav className={cls.nav}>
-        <span className={cls.linkStub}>О проекте</span>
+        <span className={cls.about}>О проекте</span>
 
         <button type="button" className={cls.skillsButton}>
-          Все навыки
+          <span>Все навыки</span>
+          <svg width="24" height="24" viewBox="0 0 24 24">
+            <path
+              d="M12 15.935a2.52 2.52 0 0 1-1.781-.738L4.2 9.179a.696.696 0 0 1 0-.978.696.696 0 0 1 .978 0l6.018 6.018a1.136 1.136 0 0 0 1.606 0L18.821 8.2a.696.696 0 0 1 .978 0 .696.696 0 0 1 0 .978l-6.018 6.018a2.5 2.5 0 0 1-1.781.738"
+              fill="currentColor"
+            />
+          </svg>
         </button>
       </nav>
     </div>
