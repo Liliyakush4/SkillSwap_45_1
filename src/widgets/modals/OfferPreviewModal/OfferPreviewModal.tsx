cf907e2e4f45faad233bf0993e9beefa2d591610
Ekaterina Kharina <Ekaterina.Kharina@waveaccess.ru>
2026-02-21 14:24:04 +0300

    исправление модального окна

--- src/widgets/modals/OfferPreviewModal/OfferPreviewModal.tsx
+++ src/widgets/modals/OfferPreviewModal/OfferPreviewModal.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Modal } from '../../../shared/ui/Modal/Modal';
 import { Button } from '../../../shared/ui/Button/Button';
-import PersonCircleIcon from '../../../shared/assets/icons/common/icon_person_circle_nosize.svg';
+import NosizeIcon from '../../../shared/assets/icons/common/icon_bell_nosize.svg';
 import styles from './OfferPreviewModal.module.css';
 
 export interface OfferPreviewModalProps {
@@ -18,12 +18,12 @@ export const OfferPreviewModal: React.FC<OfferPreviewModalProps> = ({ isOpen, on
     <Modal isOpen={isOpen} onClose={handleClose} className={styles.modal}>
       <div className={styles.container}>
         <div className={styles.iconWrapper}>
-          <img src={PersonCircleIcon} alt="" aria-hidden="true" className={styles.icon} />
+          <img src={NosizeIcon} alt="" aria-hidden="true" className={styles.icon} />
         </div>
 
-        <h2 className={styles.title}>Ваше предложение создано</h2>
+        <h2 className={styles.title}>Вы предложили обмен</h2>
 
-        <p className={styles.description}>Теперь вы можете предложить обмен</p>
+        <p className={styles.description}>Теперь дождитесь подтверждения. Вам придёт уведомление</p>
 
         <div className={styles.buttonWrapper}>
           <Button fullWidth onClick={handleClose}>
