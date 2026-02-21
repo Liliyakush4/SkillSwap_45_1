12fe9b8a40ba267ad2cbc8c4f206b01250ee926d
vi.vala.vi <sovestyaaaa@gmail.com>
2026-02-22 00:38:25 +0300

    add persist store

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import App from './app/App';
-import { store } from './app/store/store';
-import './app/styles/globals.css';
+import App from '@app/App';
+import store, { persistor } from '@app/store/store';
+import '@app/styles/globals.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>,
 );
