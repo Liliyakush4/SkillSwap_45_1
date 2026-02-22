abe0364b5d1248a39043aeb7df6879283f9f99a8
Ekaterina Kharina <Ekaterina.Kharina@waveaccess.ru>
2026-02-22 17:06:05 +0300

    поправлены расположение шагов регистрации, собрана и подключена шаг 2

---
+++ src/pages/auth/register/register-step-3/RegisterStep3Page.tsx
@@ -0,0 +1,59 @@
+import { RegisterStep3Form } from '@features/auth/ui/register-step-3-form';
+import styles from './RegisterStep3Page.module.css';
+import RegistrBoardImage from '@shared/assets/images/auth/registration_board.svg';
+import { ContentSection } from '@shared/ui/content-section/ContentSection';
+import { StepProgress } from '@shared/ui/step-progress/StepProgress';
+
+/* времененные данные чтобы проверить вывод данных в полях при регистрации*/
+const skillCategoryLearnOptions = [
+  { value: 'business', label: 'Бизнес и карьера' },
+  { value: 'creativity', label: 'Творчество и искусство' },
+  { value: 'languages', label: 'Иностранные языки' },
+  { value: 'health', label: 'Здоровье и лайфстайл' },
+  { value: 'home', label: 'Дом и уют' },
+];
+
+const skillSubcategoryLearnOptions = [
+  { value: 'drawing', label: 'Рисование и иллюстрация' },
+  { value: 'photography', label: 'Фотография' },
+  { value: 'video', label: 'Видеомонтаж' },
+  { value: 'music_sound', label: 'Музыка и звук' },
+  { value: 'acting', label: 'Актёрское мастерство' },
+  { value: 'writing', label: 'Креативное письмо' },
+  { value: 'art_therapy', label: 'Арт-терапия' },
+  { value: 'decor_diy', label: 'Декор и DIY' },
+];
+
+export const RegisterStep3Page: React.FC = () => {
+  const heroText = (
+    <div className={styles.heroContainer}>
+      <h2 className={styles.heroTitle}>Укажите, чем вы готовы поделиться</h2>
+      <p>Так другие люди смогут увидеть ваши предложения и предложить вам обмен!</p>
+    </div>
+  );
+
+  return (
+    <>
+      <StepProgress currentStep={3} totalSteps={3} className={styles.stepProgress} />
+      <ContentSection
+        main={
+          <>
+            <RegisterStep3Form
+              values={{
+                skillName: '',
+                category: [],
+                subcategory: [],
+                description: '',
+                photos: [],
+              }}
+              categoryOptions={skillCategoryLearnOptions}
+              subcategoryOptions={skillSubcategoryLearnOptions}
+            />
+          </>
+        }
+        heroText={heroText}
+        heroImage={<img src={RegistrBoardImage} alt="Картинка" />}
+      />
+    </>
+  );
+};
--- src/pages/register/register-step-3/RegisterStep3.tsx
+++
@@ -1,7 +0,0 @@
-export default function RegisterStep3() {
-  return (
-    <div>
-      <h1>Шаг регистрации 3</h1>
-    </div>
-  );
-}
