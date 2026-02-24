9fc82c0599f1bcd110b8faa4338719a7c6fb2d75
Liliya Kushnikova <iamlilkush4@gmail.com>
2026-02-24 16:04:40 +0300

    фото польпользователей в public

--- src/entities/user/ui/user-card/UserCard.tsx
+++ src/entities/user/ui/user-card/UserCard.tsx
@@ -50,7 +50,7 @@ export const UserCard: React.FC<UserCardProps> = ({
       data-height={height} // data-атрибут для управления высотой
     >
       <div className={styles.header}>
-        <Avatar src={avatarSrc} size={130} alt={`Аватар пользователя ${name}`} />
+        <Avatar src={avatarSrc} size={100} alt={`Аватар пользователя ${name}`} />
         <div className={styles.userInfo}>
           <h2 className={styles.name}>{name}</h2>
           <div className={styles.cityAge}>{age === undefined ? `${city}` : `${city}, ${age}`}</div>
