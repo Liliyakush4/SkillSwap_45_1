54839b5a4221c9d0c91161b801b1beab1eb837ba
Valentin Braun <braunvlntn@yandex.ru>
2026-02-25 23:11:40 +0500

    исправил HeaderActionsUser

--- src/widgets/Header/ui/header-actions-user/HeaderActionsUser.tsx
+++ src/widgets/Header/ui/header-actions-user/HeaderActionsUser.tsx
@@ -1,29 +1,47 @@
+import { useRef, useState } from 'react';
 import { IconButton } from '@shared/ui/icon-button';
 import cls from './HeaderActionsUser.module.css';
 import iconThemeDark from '@shared/assets/icons/ui/icon_theme_dark.svg';
 import iconHeart from '@shared/assets/icons/ui/icon_heart.svg';
 import iconBell from '@shared/assets/icons/common/icon_bell_nosize.svg';
-import { Avatar } from '@shared/ui/Avatar';
+import { NotificationsPopover } from '@widgets/popovers/notificationsPopover';
+import { ProfileMenu } from '@widgets/popovers/profile-menu/ProfileMenu.tsx';
+import { useAppSelector } from '@shared/lib/storeHooks';
+import { selectProfileName } from '@features/profile/model/selectors';
 
 export const HeaderActionsUser = () => {
+  const [isNotificationsOpen, setIsNotificationsOpen] = useState(false);
+  const bellRef = useRef<HTMLButtonElement>(null);
+  const userName = useAppSelector(selectProfileName);
+
   return (
     <div className={cls.wrapper}>
       <div className={cls.buttonGroup}>
         <IconButton icon={<img src={iconThemeDark} alt="" />} aria-label="Темная тема" />
 
         <IconButton
+          ref={bellRef}
           icon={<img src={iconBell} alt="" width={24} height={24} />}
-          aria-label="Избранное"
+          aria-label="Уведомления"
+          onClick={() => setIsNotificationsOpen((prev) => !prev)}
+          isActive={isNotificationsOpen}
         />
 
         <IconButton icon={<img src={iconHeart} alt="" />} aria-label="Избранное" />
       </div>
       <div className={cls.profile}>
-        <button type="button" className={cls.profileButton} aria-label="Профиль">
-          <span className={cls.name}>Иван</span>
-          <Avatar />
-        </button>
+        <ProfileMenu
+          userName={userName || 'Гость'}
+          profilePath="/profile"
+          onLogoutClick={() => console.log('Logout clicked')}
+        />
       </div>
+
+      <NotificationsPopover
+        isOpen={isNotificationsOpen}
+        onClose={() => setIsNotificationsOpen(false)}
+        anchorRef={bellRef}
+      />
     </div>
   );
 };
